78dae370a659b520de4e864af747f9859f12ed0c
AnnJakubel <annabel.jakubel@gmail.com>
2022-12-23 21:52:39 +0200

    Resolved 'import React from 'react'' error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from 'react-bootstrap';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pet-manager-frontend';
+  
+
 }
